11c2e3564c058e607a45728b76e78f048cf97202
Carlos <cdaniel2r.dr@gmail.com>
2019-11-17 22:25:09 -0300

    03 declarar variables

--- exercises/02-function-hello/App.java
+++ exercises/02-function-hello/App.java
@@ -1,12 +1,13 @@
 /* This is a simple Java program. */
 class App
 {
-    public Integer sum(Integer a, Integer b){
-        return 2;
+	public static Integer sum(Integer a, Integer b){
+        return a+b;
     }
     
     public static void main(String args[])
     {
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World");
+        //System.out.println(sum(5,6));
     }
 }
---
+++ exercises/03-declare-variables/README.md
@@ -0,0 +1,3 @@
+
+
+Declarar dos variables numéricas (con el valor que desees), muestra por consola la suma, resta, multiplicación, división y módulo (resto de la división).
