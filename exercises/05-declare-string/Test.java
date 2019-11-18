8eac0e6a8551c91cac852190a9f45ee57bbfd99a
Carlos <cdaniel2r.dr@gmail.com>
2019-11-18 00:01:59 -0300

    ejercicio 6

--- exercises/05-declare-string/Test.java
+++ exercises/05-declare-string/Test.java
@@ -22,7 +22,7 @@ public class Test{
     public void testAdd() {
         App a = new App();
         a.main(new String[0]);
-        int index = outContent.indexOf("Bienvenido");
+        int index = outContent.toString().indexOf("Bienvenido");
         if (index == 0){
          	assertTrue(true);
         }else{
